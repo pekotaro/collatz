99bb723a92c87d54e90be651d0853ad7d9667ac4
pekotaro <sakatatjktks@msn.com>
2014-11-27 21:50:11 +0900

    処理時間を計測する処理を追加

--- collatz.c
+++ collatz.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
+#include <time.h>
 
 typedef struct {
     int n;
     int steps;
 } result_t;
 
-void main(void) {
-    result_t result = {0,0};
-    int n, steps, max_steps = 0;
+result_t collutz_roop(int first_n, int last_n){
+    int n, steps;
+    result_t result = {0, 0};
     
-    printf("コラッツの問題を計算します。（2～100000）\r\n");
-
-    for(n = 2; n <= 100000; n++){
-        steps = collutz(n);
+    for(n = first_n; n <= last_n; n++){
+        steps = collutz((unsigned long)n);
         if(steps > result.steps){
             result.n = n;
             result.steps = steps;
         }
-    }    
-
-    printf("結果：ステップ数が多くなる初期値は\"%d\"で%dステップかかります。\r\n", result.n, result.steps);
+    }
+    
+    return result;
 }
 
-int collutz(int n){
+int collutz(unsigned long n){
     int steps;
     for(steps = 0; n != 1; steps++){
         n = (n % 2 == 0) ? n / 2 : (n * 3) + 1;
     }
     return steps;
+}
+
+void main(void) {
+    result_t result;
+    const int first_n = 2, last_n = 1000000;
+    clock_t start_time, end_time;
+    
+    printf("コラッツの問題を計算します。（%d～%d）\r\n", first_n, last_n);
+    start_time = clock();
+    result = collutz_roop(first_n, last_n);
+    end_time = clock();
+    printf("結果：ステップ数が多くなる初期値は\"%d\"で%dステップかかります。\r\n", result.n, result.steps);
+    printf("%d[ms]でした。\r\n", end_time - start_time);
 }
